b0956c3334caccb7bd5b8abc3b3751cf1d43f412
Benedict Uduogu <jike.exe@gmail.com>
2024-08-03 21:44:21 +0100

    ...

--- Personal Projects/Test Code/test.js
+++ Personal Projects/Test Code/test.js
@@ -1,11 +1,27 @@
-import readline from "node:readline";
+import inquirer from 'inquirer';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {"message" : "what is your name?",
+      "name": "userName"
+    },
+    {
+      "message" : "what is your age?",
+      "name": "userAge"
+    }
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(`Hello, my name is ${answers.userName}! and my age is ${answers.userAge}`);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+  
 
-rl.question(`What's your name?`, (name) => {
-  console.log(`Hi ${name}!`);
-  rl.close();
-});
+
--- Personal Projects/Triangle Class/index.js
+++ Personal Projects/Triangle Class/index.js
@@ -1,53 +1,57 @@
-// class Rectangle {
-//   constructor(height, width) {
+// import inqurer from "inquirer";
+
+
+// class Triangle {
+//   constructor(base, height) {
+//     this.base = base;
 //     this.height = height;
-//     this.width = width;
 //   }
-//   // Getter
+
+//   // Get Area
 //   get area() {
 //     return this.calcArea();
 //   }
-//   // Method
+
+//   //  Method
 //   calcArea() {
-//     return this.height * this.width;
-//   }
-//   *getSides() {
-//     yield this.height;
-//     yield this.width;
-//     yield this.height;
-//     yield this.width;
+//     return (this.base / 2) * this.height;
 //   }
 // }
 
-// const square = new Rectangle(10, 10);
-
-// console.log(square.area); // 100
-// console.log([...square.getSides()]); // [10, 10, 10, 10]
-
-// AREA OF A TRIANGLE: 1/2base x height
-
-class Triangle {
-  constructor(base, height) {
-    this.base = base;
-    this.height = height;
-  }
-
-  // Get Area
-  get area() {
-    return this.calcArea();
-  }
-
-  //  Method
-  calcArea() {
-    return (this.base / 2) * this.height;
-  }
-}
-
-// Receive user dimensions
-const userBase = prompt("what is the base?");
-const userHeight = prompt("what is the triangle height?");
+// // Receive user dimensions
+// const userBase = prompt("what is the base?");
+// const userHeight = prompt("what is the triangle height?");
 
 
-// Output Area of Triangle
-const output = new Triangle(userBase, userHeight);
-console.log(`${output.area} square`); // can also be: console.log(`${output.calcArea()} square`); 
+// // Output Area of Triangle
+// const output = new Triangle(userBase, userHeight);
+// console.log(`${output.area} square`); // can also be: console.log(`${output.calcArea()} square`); 
+
+import inquirer from "inquirer";
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    { message: "what is your name?", 
+      name: "userName" },
+    {
+      message: "what is your age?",
+      name: "userAge",
+    },
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(
+      `Hello, my name is ${answers.userName}! and my age is ${answers.userAge}`
+    );
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+  
+
+
