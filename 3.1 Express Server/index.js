b9d211259ccc943e2bc489865838fe8bad8a56c7
Benedict Uduogu <jike.exe@gmail.com>
2024-08-05 07:06:55 +0100

    ....

---
+++ 3.1 Express Server/index.js
@@ -0,0 +1,11 @@
+// netstat -ano | findstr "LISTENING"
+
+import express from "express";
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, ()=> {
+   console.log(`Server is running on port ${port}`);
+})
---
+++ 3.2 HTTP Requests/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+   res.send("<h1>Main Page</h1>");
+});
+
+app.get("/about", (req, res) => {
+   res.send("<h1>About Me:</h1><p>My name is Ejike</p>");
+});
+
+app.get("/contact", (req, res) => {
+   res.send("<h1>Phone:</h1><p>08163505348</p>");
+}); 
+
+app.listen(port, ()=> {
+   // console.log(`server is running on port ${port}`);
+})
+
