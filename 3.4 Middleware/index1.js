48d2a68cbd548cebc1995bd4b85f2df3cf8a703d
Benedict Uduogu <jike.exe@gmail.com>
2024-08-07 21:06:08 +0100

    ..

--- 3.4 Middleware/index1.js
+++ 3.4 Middleware/index1.js
@@ -21,7 +21,7 @@ app.post("/submit", (req, res) => {
   console.log(formInput);
   const data = JSON.stringify(formInput);
 
-  writeFile('form-data.txt', data, (err) => {
+  writeFile('formInput.txt', data, (err) => {
   if (err) throw err;
   console.log('The file has been saved!');
 }); 
