77e5f8cfb892907e603732dc3a49ff3c46111583
Benedict Uduogu <jike.exe@gmail.com>
2024-09-12 00:13:08 +0100

    EJS Tags

--- 4.1 EJS Tags/index.js
+++ 4.1 EJS Tags/index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   const data = {
     title: "EJS Tags",
     seconds: new Date().getSeconds(),
-    items: ["apple", "banana", "cherry"],
+    items: ["apple", "banana", "cherry","beans", "fishe"],
     htmlContent: "<strong>This is some strong text</strong>",
   };
   res.render("index.ejs", data);
