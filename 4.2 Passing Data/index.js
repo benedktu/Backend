0eb7ebaa88d6d947135c9b6e1cae968271bbd84f
Benedict Uduogu <jike.exe@gmail.com>
2024-09-16 08:27:39 +0100

    ..

--- 4.2 Passing Data/index.js
+++ 4.2 Passing Data/index.js
@@ -6,9 +6,28 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.use(htmlContent);
 
-app.post("/submit", (req, res) => {});
+app.get("/", (req, res) => {
+  const htmlContent = {
+    preSubmitMessage: "Write your name here:",
+    postSubmitMessage: `Hello ${formInput.fName}, There are ${fullNameLength} characters in your name.`,
+    concactenateNames: function(req, res, next) { 
+      const fullName = formInput.fName + formInput.lName;
+      const fullNameLength = fullName.length;
+      return fullNameLength;
+    }
+  }
+
+  res.render( "index.ejs");
+
+});
+
+app.post("/submit", (req, res) => {
+  const formInput = req.body;
+  console.log(formInput);
+  console.log(formInput.fName);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
