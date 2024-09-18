25742a3a82c673d36aa4512e44b519b6d4f1d48b
Benedict Uduogu <jike.exe@gmail.com>
2024-09-18 07:59:28 +0100

    added comments and stuff

--- 4.2 Passing Data/index.js
+++ 4.2 Passing Data/index.js
@@ -8,23 +8,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const preMessage = "Write your name here:";
 
 app.get("/", (req, res) => {
-     const preMessage = "Write your name here:";
-  res.render("index.ejs");
+     const preMessage = "Write your name below:";
+  res.render("index.ejs",{ preMessage });
 });
 
 app.post("/submit", (req, res) => {
-  const formInput = req.body;
-  const fullName = formInput["fName"] + formInput["lName"];
-  const fullNameLength = fullName.length;
-  const postMessage = `Hello ${formInput["fName"]}, there are ${fullNameLength} characters in your name.`;
+     
+     const formInput = req.body; // Gets the input from the user and stores it in a variable 'formInput'
 
-  res.render("index.ejs", {
-    preMessage,     
-    postMessage,
-    fullNameLength,
-  });
+     const fullName = formInput["fName"] + formInput["lName"]; // Concatenates the fName and lName string variables based on the user's input, stores it in a variable 
+     const fullNameLength = fullName.length; // Gets the length property of the fullName variable
+     const postMessage = `Hello ${formInput["fName"]}, there are ${fullNameLength} characters in your name.`;
 
-  console.log(fullNameLength);
+  res.render("index.ejs", 
+     { // Object to hold the generated variables from user input to be rendered via the EJS template         
+          preMessage,     
+          postMessage,
+          fullNameLength,
+     });
+
+  console.log(`${fullNameLength} characters`);
 });
 
 app.listen(port, () => {
